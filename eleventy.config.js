b8d582fc289e2a6937741d36b712d61986a75fc9
ikaleidoscope96 <ikaleidoscope96@gmail.com>
2026-04-07 22:46:02 -0400

    rename img to images

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/css");
-  eleventyConfig.addPassthroughCopy({"./public/": "/"});
+	eleventyConfig.addPassthroughCopy("./src/css");
+	eleventyConfig.addPassthroughCopy({"./public/": "/"});
 
 	return {
 		// Set directroty paths
